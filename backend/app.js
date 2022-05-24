f086a41f3e80e5ebdebd49c5114796d49a6b286f
Kleindal <kleindalsama@gmail.com>
2022-05-24 19:13:50 +0200

    Setup cors

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const path = require('path');
+var cors = require('cors');
 
+app.use(cors());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');

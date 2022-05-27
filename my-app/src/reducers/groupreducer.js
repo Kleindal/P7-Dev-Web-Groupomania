2d7959436a051fd49c88522ef8a7303d8990b865
Kleindal <kleindalsama@gmail.com>
2022-05-27 20:32:44 +0200

    Remove Legacy React frontend folder

--- my-app/src/reducers/groupreducer.js
+++
@@ -1,9 +0,0 @@
-const initialState = { }
-const GroupReducer = (state = initialState, action) => {
-  if (action.type == 'GET_GROUPS') {
-    return action.payload
-  }
-    return state
-}
-
-export default GroupReducer
--- my-app/src/reducers/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import GroupReducer from './groupreducer.js'
-import PostReducer from './postreducer.js'
-import UsersReducer from './usersreducer.js'
-
-export default combineReducers({GroupReducer, PostReducer, UsersReducer})

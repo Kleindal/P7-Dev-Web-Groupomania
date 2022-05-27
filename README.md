fc53842bdcdffa028ffa9f5835d0ebe608755357
Kleindal <kleindalsama@gmail.com>
2022-05-28 00:22:18 +0200

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
-Groupomania
+<p align="center">
+  <a href="#">
+    <img src="https://user.oc-static.com/upload/2022/05/25/1653474647318_icon-left-font.png" width="auto" height="192px"/>
+  </a>
+</p>
+
+<h1 align="center">
+  Web API: Créez un réseau social d’entreprise
+</h1>
+
+<p align="center">
+  Réalisation d'une <strong>Application de publication de postes</strong> avec des fonctions basiques.
+  <br />
+  <br />
+</p>
+
+## 👩‍💻 Explication du projet
+
+L'objectif de ce projet est de créer une API reprendrant les concepts de base d'une application de publication de posts (type Workplace, Reddit, 9gag), pour l'entreprise Groupomania. Les utilisateurs pourront alors publier leurs propres posts pouvant contenir à la fois du texte et des images.
+
+Ce projet a été réalisé en :
+   - [NodeJS](https://nodejs.org/en/about/releases/)
+   - [Express](https://expressjs.com/fr/starter/installing.html)
+
+
+## 🚀 Requirement
+- npm@8.6.0
+- node@v16.13.1
+- nodemon@2.0.15
+- Une base de donnée mysql8.0
+    - L'utilisation de MySQLWorkbench est recommandé pour manipuler la base de donnée MySQL
+
+### 🐳 Installation
+Une fois l'enrivonnement installé :
+```
+git clone https://github.com/Kleindal/P7-Dev-Web-Groupomania.git
+cd P7-Dev-Web-Groupomania/backend
+npm install
+```
+
+Mise en place de la base de données:
+Éxécuter dans votre base de données le sql présent dans le fichier `./bd-config/db.models.sql`
+
+Fichier de configuration de la base de données :  
+`./bd-config/database.js`
+
+### 🚀 Démarrer l'application
+Démarrer le server backend
+```ssh
+npm run dev
+```
+Le serveur sera accessible sur l'url http://localhost:3006/  
+Les identifiants de l'administrateur sont :
+- email : admin@groupomania.fr
+- password : admin
+
+
--- backend/bd-config/db.models.sql
+++ backend/bd-config/db.models.sql
@@ -1,56 +1,40 @@
-CREATE DATABASE IF NOT EXISTS `groupomania`;
-USE `groupomania`;
+CREATE DATABASE IF NOT EXISTS `groupomania_v2`;
+
+USE `groupomania_v2`;
+
+CREATE TABLE IF NOT EXISTS `post`(
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+	`group_id` INT NOT NULL, 
+    `title` VARCHAR(60) NOT NULL,
+    `image_url` TEXT,
+	`body` TEXT NOT NULL,
+    `created_by` INT NOT NULL, 
+	`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+	`updated_at` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS `group` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `title` VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE IF NOT EXISTS `user`(
     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
     `email` VARCHAR(50) NOT NULL UNIQUE,
     `password` VARCHAR(150) NOT NULL,
     `name` VARCHAR(50) NOT NULL,
     `surname` VARCHAR(50) NOT NULL, 
+    `image_url`TEXT,
 	`is_admin` TINYINT NOT NULL DEFAULT '0',
     `has_accepted_cgu` TINYINT NOT NULL DEFAULT '0'
 );
 
-CREATE TABLE IF NOT EXISTS `invitation_token` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `created_by` INT NOT NULL, 
+CREATE TABLE IF NOT EXISTS `like_post`(
+  	`post_id` INT NOT NULL, 
     `user_id` INT NOT NULL,
-    CONSTRAINT `Created_by` FOREIGN KEY(`created_by`) REFERENCES `user` (`id`) ON DELETE CASCADE,
-    CONSTRAINT `User_id` FOREIGN KEY(`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE
+    PRIMARY KEY (`post_id`, `user_id`)
 );
 
-CREATE TABLE IF NOT EXISTS `group` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `title` VARCHAR(50) NOT NULL,
-    `created_by` INT NOT NULL
-);
+INSERT INTO user (email, password, name, surname, is_admin, has_accepted_cgu)
+VALUES ('admin@groupomania.fr', '$2b$10$feBk8.epmNVIbAVtYrFaVOxkYBRyCePeaNY6hyzCxouFUWEepKlaG', 'Admin', 'Admin', 1, 1);
 
-CREATE TABLE IF NOT EXISTS `group_user` (
-    `group_id` INT NOT NULL,
-    `user_id` INT NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS `message` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `author_id` INT NOT NULL, 
-    `group_id` INT NOT NULL,
-    `text` TEXT NOT NULL,
-    CONSTRAINT `Group_id` FOREIGN KEY(`group_id`) REFERENCES `group` (`id`) ON DELETE CASCADE
-    -- `like_user` INT NOT NULL
-);
-
--- CREATE TABLE IF NOT EXISTS `comment` (
---     `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
---     `message_id` INT NOT NULL, 
---     `like` INT NOT NULL, 
---     `dislike` INT NOT NULL, 
---     CONSTRAINT `Like` FOREIGN KEY(`like`) REFERENCES `message` (`id`) ON DELETE CASCADE,
---     CONSTRAINT `Dislike` FOREIGN KEY(`dislike`) REFERENCES `message` (`id`) ON DELETE CASCADE,
---     CONSTRAINT `Message_id` FOREIGN KEY(`message_id`) REFERENCES `message` (`id`) ON DELETE CASCADE
--- );
-
-CREATE TABLE IF NOT EXISTS `attachement` (
-    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    `filename` VARCHAR(255) NOT NULL,
-    `message_id` INT NOT NULL,
-    CONSTRAINT `Message_id` FOREIGN KEY(`message_id`) REFERENCES `message` (`id`) ON DELETE CASCADE
-);
